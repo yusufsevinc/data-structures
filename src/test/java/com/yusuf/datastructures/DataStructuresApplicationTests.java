a701d470454c6d3c00374cb97add895e2ad54ffd
yusufsevinc <yusuf.sevinc@inomera.com>
2023-01-07 17:01:41 +0300

    create project

---
+++ src/test/java/com/yusuf/datastructures/DataStructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusuf.datastructures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
